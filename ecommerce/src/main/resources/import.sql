856740741d3860eb85f021a2f6eecdaca50aa15c
Noesse1 <272362@unifio.edu.br>
2026-09-03 01:12:33 +0000

    Atualizações

--- ecommerce/src/main/resources/import.sql
+++ ecommerce/src/main/resources/import.sql
@@ -3,23 +3,17 @@ insert into categoria (nome, descricao) values ('Livros', 'Livros Técnicos');
 
 insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Código Limpo', 'Livro do Autor Robert C. Martin', 87.34, 20, 2);
 
-INSERT INTO cliente (nome, email, telefone) VALUES ('Ana Beatriz', 'ana@email.com', '11999999999');
-INSERT INTO cliente (nome, email, telefone) VALUES ('João Marcos', 'joao@email.com', '11988888888');
-INSERT INTO cliente (nome, email, telefone) VALUES ('Mariana Silva', 'mariana@email.com', '11977777777');
-INSERT INTO cliente (nome, email, telefone) VALUES ('Carlos Eduardo', 'carlos@email.com', '11966666666');
-INSERT INTO cliente (nome, email, telefone) VALUES ('Lucas Lima', 'Lucas@email.com', '11955555555');
+INSERT INTO clientes (nome, email, telefone) VALUES ('Ana Beatriz', 'ana@email.com', '11999999999');
+INSERT INTO clientes (nome, email, telefone) VALUES ('João Marcos', 'joao@email.com', '11988888888');
+INSERT INTO clientes (nome, email, telefone) VALUES ('Mariana Silva', 'mariana@email.com', '11977777777');
+INSERT INTO clientes (nome, email, telefone) VALUES ('Carlos Eduardo', 'carlos@email.com', '11966666666');
+INSERT INTO clientes (nome, email, telefone) VALUES ('Lucas Lima', 'Lucas@email.com', '11955555555');
 
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-01T10:00:00', 'AGUARDANDO', 4500.00, 1);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-01T11:30:00', 'PAGO', 2500.00, 2);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-02T09:15:00', 'ENVIADO', 3000.00, 3);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-02T14:20:00', 'ENTREGUE', 4000.00, 4);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-02T16:45:00', 'CANCELADO', 1200.00, 5);
-
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-01T10:00:00', 'AGUARDANDO', 4500.00, 1);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-01T11:30:00', 'PAGO', 2500.00, 2);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-02T09:15:00', 'ENVIADO', 3000.00, 3);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-02T14:20:00', 'ENTREGUE', 4000.00, 4);
-INSERT INTO pedido (data, status, valor_total, cliente_id) VALUES ('2026-09-02T16:45:00', 'CANCELADO', 1200.00, 5);
+INSERT INTO pedidos (data, status, valor_total, cliente_id) VALUES ('2026-09-01T10:00:00', 'AGUARDANDO', 4500.00, 1);
+INSERT INTO pedidos (data, status, valor_total, cliente_id) VALUES ('2026-09-01T11:30:00', 'PAGO', 2500.00, 2);
+INSERT INTO pedidos (data, status, valor_total, cliente_id) VALUES ('2026-09-02T09:15:00', 'ENVIADO', 3000.00, 3);
+INSERT INTO pedidos (data, status, valor_total, cliente_id) VALUES ('2026-09-02T14:20:00', 'ENTREGUE', 4000.00, 4);
+INSERT INTO pedidos (data, status, valor_total, cliente_id) VALUES ('2026-09-02T16:45:00', 'CANCELADO', 1200.00, 5);
 
 INSERT INTO item_pedido (quantidade, valor_unitario, pedido_id, produto_id) VALUES (1, 4500.00, 1, 1);
 INSERT INTO item_pedido (quantidade, valor_unitario, pedido_id, produto_id) VALUES (1, 2500.00, 2, 2);
